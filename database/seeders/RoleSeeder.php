f2fe6352d4c500e4ea676711e334e44b4975066b
Edgar147 <karapetyane1996@gmail.com>
2022-02-19 21:01:02 +0100

    First Seeding

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Role;
+use Illuminate\Support\Facades\DB;
+
 
 class RoleSeeder extends Seeder
 {
@@ -13,6 +16,9 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Role::factory()->times(10)->create(); if generate fake roles
+        DB::table('roles')->insert(['name'=>'SuperAdmin']);
+        DB::table('roles')->insert(['name'=>'Admin']);
+        DB::table('roles')->insert(['name'=>'User']);
     }
 }
